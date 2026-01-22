a31c2aee92e7de20fc62a0ec0c9c99ce94a138f6
fsfesdsfs <68592763+fsfesdsfs@users.noreply.github.com>
2026-01-22 13:04:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 st.title("Prilojenie za vuvejdane na tekst")
-st.text_input
-st.number_input     
-st.selectbox
-st.button
+st.text_input()
+st.number_input()     
+st.selectbo()
+st.button()
+st.succes()
+st.warning()
